d82190d29676a8b02af1fd94049b7f1e8a81babf
Jakub Zarzeczny <jkzarzeczny@gmail.com>
2023-04-22 19:39:32 +0200

    step 2

---
+++ src/lib/components/Form.svelte
@@ -0,0 +1,51 @@
+<div class="container__form">
+	<form class="form" action="">
+		<input
+			type="text"
+			name="todo"
+			id="todo"
+			class="form__control__input"
+			placeholder="Add new task"
+		/>
+
+		<input class="form__submit" type="button" value="Add" />
+	</form>
+</div>
+
+<style lang="scss">
+	@use '../styles/colors.scss' as *;
+
+	.container__form {
+		width: 100%;
+		padding: 1rem 0;
+		.form {
+			display: flex;
+			justify-content: center;
+			width: 100%;
+		}
+		.form__control__input {
+			width: 400px;
+			outline: none;
+			border: none;
+			background-color: $primary-color;
+			color: #fff;
+			padding: 8px;
+			border-radius: 5px 0 0 5px;
+			border: 1px solid $background-color;
+
+			&:focus {
+				outline: none;
+			}
+		}
+		.form__submit {
+			display: block;
+			padding: 6px 18px;
+			background-color: $background-color;
+			color: $text-color;
+			border: none;
+			border-radius: 0 5px 5px 0;
+			cursor: pointer;
+			position: relative;
+		}
+	}
+</style>
---
+++ src/lib/components/items/Items.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Item from './Item.svelte';
+</script>
+
+<div class="container__items">
+	<Item />
+</div>
+
+<style lang="scss">
+	@use '../../styles/colors.scss' as *;
+
+	.container__items {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+		width: 100%;
+		align-items: center;
+		margin-bottom: 2rem;
+	}
+</style>
